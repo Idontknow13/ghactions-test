03610c71cc4e0853d7ab55bd4186b734aa3d4f3f
Idontknow13 <pokeminecraftexpert@gmail.com>
2023-07-17 16:06:47 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ghactions-test
 Testing publishing releases and working with Github Actions.
+
+## About
+
+This repository contains a CLI application that screams at you using a certain amount of characters that you provide [see `Usage`]. Everytime a release tag is committed, the application will be built into a Linux x86-64 binary via Github Actions. The build artifacts should be inside the tag and downloadable as necessary.
+
+## Usage
+
+`scream 100` : screams using 100 characters.
---
+++ src/screamer.rs
@@ -0,0 +1,26 @@
+fn create_scream(max_len: usize) -> String {
+    let scream_len = fastrand::usize(..=max_len);
+    let excl_len = max_len - scream_len;
+
+    format!("{}{}", "A".repeat(scream_len), "!".repeat(excl_len))
+}
+
+fn main() {
+    let mut numargs = std::env::args().filter_map(|arg| arg.parse::<usize>().ok());
+    if let Some(chars) = numargs.next() {
+        println!("{}", create_scream(chars));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_scream() {
+        let len = 10;
+        let scream = create_scream(len);
+
+        assert_eq!(len, scream.len())
+    }
+}
